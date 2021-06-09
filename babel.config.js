35fe2062085cdccd8780398b730caeae6f7ebe78
EGOIST <0x142857@gmail.com>
2021-06-09 22:57:03 +0800

    remove why-did-you-render

--- babel.config.js
+++ babel.config.js
@@ -1,20 +1,5 @@
 module.exports = function (api) {
-  const isServer = api.caller((caller) => caller && caller.isServer)
-  const isCallerDevelopment = api.caller((caller) => caller && caller.isDev)
-
-  const presets = [
-    [
-      'next/babel',
-      {
-        'preset-react': {
-          importSource:
-            !isServer && isCallerDevelopment
-              ? '@welldone-software/why-did-you-render'
-              : 'react',
-        },
-      },
-    ],
-  ]
+  const presets = [['next/babel']]
 
   const plugins = ['superjson-next']
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,6 @@ import '../css/prose.css'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 
-if (process.browser && process.env.NODE_ENV === 'development') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render')
-  whyDidYouRender(React, {
-    trackAllPureComponents: true,
-  })
-}
-
 const App = ({ Component, pageProps }: any) => {
   const queryClient = React.useMemo(() => new QueryClient(), [])
 
