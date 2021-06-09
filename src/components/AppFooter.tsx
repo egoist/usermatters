6f45cf16f5b3732f542de281b521b91f25e57a84
EGOIST <0x142857@gmail.com>
2021-06-10 00:47:15 +0800

    add link to github

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,7 +1,21 @@
 export const AppFooter = () => {
+  const links = [
+    { text: 'GitHub', href: 'https://github.com/egoist/usermatters' },
+  ]
   return (
     <footer className="border-t py-20 mt-20">
-      <div className="max-w-3xl mx-auto px-5">&copy; 2021 User Matters.</div>
+      <div className="max-w-3xl mx-auto px-5 flex justify-between items-center">
+        <span>&copy; 2021 User Matters.</span>
+        <ul className="flex space-x-3 text-gray-500 hover:text-black">
+          {links.map((link) => (
+            <li key={link.text}>
+              <a target="_blank" href={link.href}>
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </footer>
   )
 }
